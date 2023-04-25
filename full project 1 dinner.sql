13e979ed992de401f629b6a795d4cd4778a41382
ANALYSTDIKSHA <126346809+ANALYSTDIKSHA@users.noreply.github.com>
2023-04-25 14:04:51 +0530

    Add files via upload

--- full project 1 dinner.sql
+++ full project 1 dinner.sql
@@ -16,7 +16,8 @@ group by customer_id ;
 
 # What was the first item from the menu purchased by each customer?
 with cte1 as
-(select m.product_name,m.price,s.customer_id,s.order_date,s.product_id , dense_rank() over (partition by customer_id order by order_date asc) as ranks
+(select m.product_name,m.price,s.customer_id,s.order_date,s.product_id , 
+dense_rank() over (partition by customer_id order by order_date asc) as ranks
 from sales as s 
 join menu as m
 on m.product_id = s.product_id) 
@@ -31,7 +32,7 @@ select m.product_name,count(*) as number_of_times_purchased
 from sales as s
 join menu as m
 on m.product_id=s.product_id
-group by s.product_id
+group by m.product_name
 order by number_of_times_purchased desc limit 1 ;
 
 
@@ -40,12 +41,13 @@ select m.product_name,s.customer_id ,count(*) as number_of_times_purchased
 from sales as s
 join menu as m
 on m.product_id=s.product_id
-group by s.product_id ,s.customer_id
-order by product_name, number_of_times_purchased desc ;
+group by m.product_name ,s.customer_id
+order by s.customer_id, number_of_times_purchased desc ;
 
 
 # What is the most purchased item on the menu and how many times was it purchased by each customer?
-with cte1 as(select m.product_name,m.price,s.customer_id,s.order_date,s.product_id ,dense_rank() over(order by s.product_id desc) as ranks
+with cte1 as(select m.product_name,m.price,s.customer_id,s.order_date,s.product_id ,
+dense_rank() over(order by s.product_id desc) as ranks
 from sales as s 
 join menu as m
 on m.product_id = s.product_id),
@@ -53,40 +55,41 @@ cte2 as (select product_id,count(*) from cte1 group by product_id )
 select product_name,customer_id,count(*)
 from cte1
 where ranks=1
-group by customer_id ;
+group by PRODUCT_NAME,customer_id ;
 
 
-# Which item was the most popular for each customer ?
+# Which item was the most popular for each customer?
 with cte1 as(
-select m.product_name,m.price,s.customer_id,s.order_date,s.product_id ,count(*) as total_units
+select m.product_name,s.customer_id,count(*) as total_units
 from sales as s 
 join menu as m
 on m.product_id = s.product_id
-group by s.customer_id , m.product_id) ,
+group by s.customer_id , m.product_name) ,
 cte2 as(
 select * , dense_rank() over (partition by customer_id order by total_units desc) as ranks
 from cte1)
-select customer_id,product_name,price,total_units
+select customer_id,product_name,total_units
  from cte2 
  where ranks =1 ;
  
  
 # Which item was purchased first by the customer after they became a member?
-select * 
-from sales as s
-join members as m
-on m.customer_id = s.customer_id
-join menu as me
-on me.product_id = s.product_id
-where s.order_date >= m.join_date 
-group by s.customer_id
-order by s.order_date asc ;
+#select s.customer_id, me.product_name,s.order_date,m.join_date
+#from sales as s
+#join members as m
+#on m.customer_id = s.customer_id
+#join menu as me
+#on me.product_id = s.product_id
+#where s.order_date >= m.join_date 
+#group by s.customer_id, me.product_name,s.order_date,m.join_date
+#order by s.order_date desc ;
 
 
 # Which item was purchased first by the customer after they became a member?
 # ALTERNATIVE WAY--
 with cte1 as(
-select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date , dense_rank() over(partition by s.customer_id order by s.order_date asc) as ranks
+select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date ,
+ dense_rank() over(partition by s.customer_id order by s.order_date asc) as ranks
 from sales as s
 join members as m
 on m.customer_id = s.customer_id
@@ -95,13 +98,12 @@ on me.product_id = s.product_id
 where s.order_date >= m.join_date)
 select customer_id,product_name , order_date , join_date
 from cte1 where ranks =1; 
-#group by s.customer_id
-#order by s.order_date asc 
 
 
 # Which item was purchased just before the customer became a member?
 with cte1 as(
-select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date , dense_rank() over(partition by s.customer_id order by s.order_date desc) as ranks
+select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date ,
+ dense_rank() over(partition by s.customer_id order by s.order_date desc) as ranks
 from sales as s
 join members as m
 on m.customer_id = s.customer_id
@@ -113,15 +115,31 @@ from cte1
  where ranks =1; 
 
 
-# What is the total items and amount spent for each member before they became a member?
+# What is the total items and amount spent for each member before they became a member including the date they joined?
 with cte1 as(
-select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date , dense_rank() over(partition by s.customer_id order by s.order_date desc) as ranks
+select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date ,
+ dense_rank() over(partition by s.customer_id order by s.order_date desc) as ranks
 from sales as s
 join members as m
 on m.customer_id = s.customer_id
 join menu as me
 on me.product_id = s.product_id
 where s.order_date <= m.join_date)
+select customer_id ,sum(price) ,count(*)
+# customer_id,product_name , order_date , join_date
+from cte1
+group by customer_id ;
+
+# What is the total items and amount spent for each member before they became a member?
+with cte1 as(
+select me.product_name,me.price,s.customer_id,s.order_date,s.product_id,m.join_date ,
+ dense_rank() over(partition by s.customer_id order by s.order_date desc) as ranks
+from sales as s
+join members as m
+on m.customer_id = s.customer_id
+join menu as me
+on me.product_id = s.product_id
+where s.order_date < m.join_date)
 select customer_id ,sum(price) ,count(*)
 # customer_id,product_name , order_date , join_date
 from cte1
